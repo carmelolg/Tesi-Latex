35cf31bdcd7e124b57241dcf1c2d3f678f2c8a4d
Carmelo La Gamba <carmelolagamba@hotmail.it>
2015-05-14 18:19:16 +0200

    Hybrid.

--- Tesi/code/host_h_device.c
+++ Tesi/code/host_h_device.c
@@ -1,5 +1,5 @@
 //Using cudaMemcpy
-cudaMemcpy(ibrid_model->i,host_model->i, sizeof(CALint)*model->sizeof_X, cudaMemcpyHostToDevice);
+cudaMemcpy(hybrid_model->i,host_model->i, sizeof(CALint)*model->sizeof_X, cudaMemcpyHostToDevice);
 
 //Whole model passed between host and device
-cudaMemcpy(ibrid_model,host_model, sizeof(CudaCALModel2D), cudaMemcpyHostToDevice);
+cudaMemcpy(hybrid_model,host_model, sizeof(CudaCALModel2D), cudaMemcpyHostToDevice);
